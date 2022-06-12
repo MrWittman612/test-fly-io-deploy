dfd31fa8637c84c8364b49fb519b0647232c7087
Chad <40812274+MrWittman612@users.noreply.github.com>
2022-06-12 17:17:56 -0500

    centered text

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,15 @@
+const styles = {
+  fontFamily: 'system-ui, sans-serif',
+  lineHeight: '1.4',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh',
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+    <div style={styles}>
       <h1>Welcome to Remix</h1>
     </div>
   );
